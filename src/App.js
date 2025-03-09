1611f315380bfae530ab55ed48cc53e0c178875e
David T. <davidt.info@gmail.com>
2025-03-09 11:56:47 -0300

    Usando state para resolver nome que não atualiza na tela

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
 
-  let nome = 'Vinny'
+  const [nome, setNome] = useState('Vinny')
 
   function alterarNome () {
-    nome = 'Vinicios Neves'
+    setNome('Vinicios Neves')
     console.log(nome)
   }
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{nome}</h1>
-        <button onClick={alterarNome}>Mudar para nome completo</button>
       </header>
     </div>
   );
