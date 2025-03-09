665393156ad6f503463b76281f0dc05fe0c0fcf5
David T. <davidt.info@gmail.com>
2025-03-09 12:30:50 -0300

    Exemplo inicial de busca de CEP

--- src/App.js
+++ src/App.js
@@ -5,15 +5,41 @@ import { useState } from 'react';
 
 function App() {
 
-  const [nome, setNome] = useState('Vinny')
+  const [endereco, setEndereco] = useState({})
 
-  function alterarNome () {
-    setNome('Vinicios Neves')
-    console.log(nome)
+  function manipularEndereco (evento) {
+    const cep = evento.target.value
+
+    setEndereco({
+      cep
+    })
+
+    if (cep && cep.length === 8) {
+      //obter o cep
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco({
+            cep: cep,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+          })
+        })
+    }
   }
+
   return (
     <div className="App">
       <header className="App-header">
+        <input placeholder='Digite o cep' onChange={manipularEndereco} />
+        <ul>
+          <li>Cep: {endereco.cep}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
       </header>
     </div>
   );
